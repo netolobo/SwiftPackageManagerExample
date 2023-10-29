27ce1e86cfb360dfa86fa884bfc33ec221300e06
netolobo <desilio@gmail.com>
2023-10-29 12:58:01 -0700

    Initial Commit

---
+++ SwiftPackageManagerExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPackageManagerExample
+//
+//  Created by Neto Lobo on 29/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPackageManagerExample/SwiftPackageManagerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackageManagerExampleApp.swift
+//  SwiftPackageManagerExample
+//
+//  Created by Neto Lobo on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackageManagerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
